d32cab3cb9eb5e5b1702b836f74ee7a4adf68119
ronalyn05 <rogidmarabi@gmail.com>
2024-07-26 18:45:48 +0800

    adjusted the base

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ const App = () => {
       <div>
         <Navbar />
         <Routes>
-          <Route path="/home" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/footer" element={<Footer />} />
