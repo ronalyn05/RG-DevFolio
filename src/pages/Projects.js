489f67c82a448d69fd7f5bda26a0b582037f2818
ronalyn05 <rogidmarabi@gmail.com>
2024-07-26 18:55:45 +0800

    adjusted some of the description

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -6,10 +6,7 @@ const Projects = () => {
   return (
     <div>
       <h2>Projects</h2>
-      <p>
-        Details about your projects will go here. You can showcase different
-        projects and their descriptions.
-      </p>
+      <p>Details about my projects will go here. Just wait, okay!!??</p>
       {/* Footer */}
       <Footer />
     </div>
