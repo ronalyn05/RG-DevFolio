4b1e82bbf64d990174cc6560c0e7d4d7d6f33f54
ronalyn05 <rogidmarabi@gmail.com>
2024-07-28 03:30:55 +0800

    added projects content and some of the small changes in ui

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,64 +1,211 @@
-// src/pages/About.js
 import React from "react";
+import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 import Footer from "../components/Footer.js";
+
+// Sample logos, you need to replace these with actual paths or URLs to logos
+const logos = {
+  Javascript:
+    "https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png",
+  Html: "https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg",
+  CSS: "https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg",
+  C: "https://upload.wikimedia.org/wikipedia/commons/1/19/C_Logo.png",
+  CSharp: "https://upload.wikimedia.org/wikipedia/commons/4/4f/Csharp_Logo.png",
+  PHP: "https://upload.wikimedia.org/wikipedia/commons/2/27/PHP-logo.svg",
+  ReactJS: "https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg",
+  ASPNet:
+    "https://upload.wikimedia.org/wikipedia/commons/e/ee/.NET_Core_Logo.svg",
+  Laravel: "https://upload.wikimedia.org/wikipedia/commons/9/9a/Laravel.svg",
+  VSCode:
+    "https://upload.wikimedia.org/wikipedia/commons/9/9a/Visual_Studio_Code_1.35_icon.svg",
+  VisualStudio:
+    "https://upload.wikimedia.org/wikipedia/commons/5/59/Visual_Studio_Icon_2019.svg",
+  Figma: "https://upload.wikimedia.org/wikipedia/commons/3/33/Figma-logo.svg",
+  Postman:
+    "https://upload.wikimedia.org/wikipedia/commons/c/c2/Postman_%28software%29.png",
+  SublimeText:
+    "https://upload.wikimedia.org/wikipedia/en/d/d2/Sublime_Text_3_logo.png",
+  Github:
+    "https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg",
+  MSSQL:
+    "https://upload.wikimedia.org/wikipedia/commons/8/87/Sql_data_base_with_logo.png",
+  Firebase:
+    "https://upload.wikimedia.org/wikipedia/commons/4/45/Firebase_Logo.svg",
+  MySQL: "https://upload.wikimedia.org/wikipedia/en/d/dd/MySQL_logo.svg",
+};
 
 const About = () => {
   return (
-    <div>
-      <main>
-        <h2>About Me</h2>
-        <h4>Hello there!</h4>
-        <p>
-          I'm a passionate Full Stack Web Developer with hands-on experience
-          gained through internships and academic projects. During my
-          internship, I honed my skills in front-end and back-end development,
-          gaining proficiency in technologies such as HTML, CSS, JavaScript,
-          Node.js, and ReactJS. As for the database, I used MSSQL.
-        </p>
-        <p>
-          My capstone project showcases my ability to design and develop
-          scalable web applications that meet business needs effectively. I
-          leveraged frameworks like ASP.net (web form - based application) and
-          Firebase to create responsive and dynamic user interfaces, alongside
-          implementing robust server-side functionalities.
-        </p>
-        <p>
-          Throughout my academic journey, I undertook various school projects
-          that further enriched my understanding of software development
-          principles. These projects allowed me to experiment with different
-          technologies, improving my problem-solving skills and creativity.
-        </p>
-        <p>
-          I am committed to continuous learning and staying updated with the
-          latest technologies in web development. I am eager to contribute my
-          skills and expand my knowledge in a dynamic and innovative
-          environment. My goal is to build impactful solutions and continue
-          growing in my career.
-        </p>
-        <strong>
-          Let's connect and explore opportunities to create impactful solutions
-          together!
-        </strong>
-
-        <strong>My technology skillset includes:</strong>
-        <ul>
-          <li>
-            <strong>Languages:</strong> Javascript, Html, CSS, C, C#, PHP
-          </li>
-          <li>
-            <strong>Frameworks:</strong> ReactJS, ASP.Net Web Form, Laravel
-          </li>
-          <li>
-            <strong>Tools:</strong> VS Code, Visual Studio, Figma, Postman,
-            Sublime Text, Github
-          </li>
-          <li>
-            <strong>Databases:</strong> MSSQL, NoSql (Firebase), MySQL
-          </li>
-        </ul>
-      </main>
-      {/* Footer */}
-      <Footer />
+    <div className="d-flex flex-column min-vh-100">
+      <Container className="flex-grow-1 my-4">
+        <main>
+          <Row>
+            <Col lg={8}>
+              <Row className="mb-4">
+                <Col lg={12}>
+                  <Card className="mb-4">
+                    <Card.Body>
+                      <Card.Title>About Me</Card.Title>
+                      <Card.Text>
+                        I'm a passionate Full Stack Web Developer with hands-on
+                        experience gained through internships and academic
+                        projects. During my internship, I honed my skills in
+                        front-end and back-end development, gaining proficiency
+                        in technologies such as HTML, CSS, JavaScript, Node.js,
+                        and ReactJS. As for the database, I used MSSQL.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col lg={12}>
+                  <Card className="mb-4">
+                    <Card.Body>
+                      <Card.Text>
+                        My capstone project showcases my ability to design and
+                        develop scalable web applications that meet business
+                        needs effectively. I leveraged frameworks like ASP.net
+                        (web form - based application) and Firebase to create
+                        responsive and dynamic user interfaces, alongside
+                        implementing robust server-side functionalities.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col lg={12}>
+                  <Card className="mb-4">
+                    <Card.Body>
+                      <Card.Text>
+                        Throughout my academic journey, I undertook various
+                        school projects that further enriched my understanding
+                        of software development principles. These projects
+                        allowed me to experiment with different technologies,
+                        improving my problem-solving skills and creativity.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+                <Col lg={12}>
+                  <Card className="mb-4">
+                    <Card.Body>
+                      <Card.Text>
+                        I am committed to continuous learning and staying
+                        updated with the latest technologies in web development.
+                        I am eager to contribute my skills and expand my
+                        knowledge in a dynamic and innovative environment. My
+                        goal is to build impactful solutions and continue
+                        growing in my career.
+                      </Card.Text>
+                    </Card.Body>
+                  </Card>
+                </Col>
+              </Row>
+            </Col>
+            <Col lg={4}>
+              <Card>
+                <Card.Body>
+                  <Card.Title>My Technology Skillset Includes:</Card.Title>
+                  <ListGroup>
+                    <ListGroup.Item>
+                      <strong>Languages:</strong>
+                      <div>
+                        {[
+                          "Javascript",
+                          "Html",
+                          "CSS",
+                          "C",
+                          "CSharp",
+                          "PHP",
+                        ].map((tech) => (
+                          <a
+                            key={tech}
+                            href={`https://www.google.com/search?q=${tech}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="me-3"
+                          >
+                            <img
+                              src={logos[tech]}
+                              alt={`${tech} logo`}
+                              style={{ width: "40px", height: "40px" }}
+                            />
+                          </a>
+                        ))}
+                      </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <strong>Frameworks:</strong>
+                      <div>
+                        {["ReactJS", "ASPNet", "Laravel"].map((tech) => (
+                          <a
+                            key={tech}
+                            href={`https://www.google.com/search?q=${tech}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="me-3"
+                          >
+                            <img
+                              src={logos[tech]}
+                              alt={`${tech} logo`}
+                              style={{ width: "40px", height: "40px" }}
+                            />
+                          </a>
+                        ))}
+                      </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <strong>Tools:</strong>
+                      <div>
+                        {[
+                          "VSCode",
+                          "VisualStudio",
+                          "Figma",
+                          "Postman",
+                          "SublimeText",
+                          "Github",
+                        ].map((tech) => (
+                          <a
+                            key={tech}
+                            href={`https://www.google.com/search?q=${tech}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="me-3"
+                          >
+                            <img
+                              src={logos[tech]}
+                              alt={`${tech} logo`}
+                              style={{ width: "40px", height: "40px" }}
+                            />
+                          </a>
+                        ))}
+                      </div>
+                    </ListGroup.Item>
+                    <ListGroup.Item>
+                      <strong>Databases:</strong>
+                      <div>
+                        {["MSSQL", "Firebase", "MySQL"].map((tech) => (
+                          <a
+                            key={tech}
+                            href={`https://www.google.com/search?q=${tech}`}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="me-3"
+                          >
+                            <img
+                              src={logos[tech]}
+                              alt={`${tech} logo`}
+                              style={{ width: "40px", height: "40px" }}
+                            />
+                          </a>
+                        ))}
+                      </div>
+                    </ListGroup.Item>
+                  </ListGroup>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        </main>
+      </Container>
+      <Footer className="mt-auto" />
     </div>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,14 +1,77 @@
-// src/pages/Projects.js
 import React from "react";
-import Footer from "../components/Footer.js";
+import { Container, Row, Col, Card, Button } from "react-bootstrap";
+import Footer from "../components/Footer";
+import { BiLinkExternal } from "react-icons/bi";
+import projectImage1 from "../assets/image3.png"; // Replace with your project image path
+import projectImage2 from "../assets/image2.jpg"; // Replace with your project image path
+import projectImage3 from "../assets/mobile.png"; // Add another image for the first project
+import projectImage4 from "../assets/mobile-2big.png"; // Add another image for the first project
+import projectImage5 from "../assets/mobile-maps.png"; // Add another image for the first project
+import "./Project.css";
 
 const Projects = () => {
+  const projects = [
+    {
+      title: "2BIG - A Water Refilling Station Management System",
+      description:
+        "The 2BiG: Water Refilling Station Management System is a mobile and web-based application designed to efficiently manage water refilling businesses. It offers features that focuses mainly on management for product and order (both online and walk-in order), delivery details and tracking, loyalty programs, employee account and records, report viewing, and inventory control.",
+      imgPaths: [projectImage1, projectImage3, projectImage4, projectImage5],
+      link: "https://github.com/ronalyn05/WebApp-2big",
+    },
+    {
+      title: "HRIS_EmployeeDB",
+      description:
+        "This application is designed for HRAdmin. The features for an HRAdmin dashboard include secure login with password change prompts, employee data upload with automatic account creation and dynamic password generation via email, update functionalities for employee information, analytics for new hires and registered employees, filtering by status and facilities, search by name or ID, access control for salary rate visibility, employee report viewing and downloading, and additional features like forgot password and address mapping. Technologies used: React.js (Front-end), Node.js (Back-end), MSSQL (Database)",
+      imgPath: projectImage2,
+      link: "https://github.com/ronalyn05/iAssist2.0_EmployeeDB",
+    },
+  ];
+
   return (
-    <div>
-      <h2>Projects</h2>
-      <p>Details about my projects will go here. Just wait, okay!!??</p>
-      {/* Footer */}
-      <Footer />
+    <div className="d-flex flex-column min-vh-50">
+      <h1 className="project-heading">
+        My Recent <strong className="purple">Works</strong>
+      </h1>
+      <h3 className="project-subheading">
+        Here are a few projects I've worked on recently.
+      </h3>
+      <Container className="flex-grow-1 my-4 project-section">
+        <Row className="justify-content-center">
+          {projects.map((project, index) => (
+            <Col md={6} lg={4} className="mb-4" key={index}>
+              <Card className="project-card-view">
+                {Array.isArray(project.imgPaths) ? (
+                  <div className="horizontal-scroll-container">
+                    {project.imgPaths.map((imgPath, idx) => (
+                      <img
+                        key={idx}
+                        src={imgPath}
+                        alt={`Screenshot ${idx + 1} of ${project.title}`}
+                        className="project-image"
+                      />
+                    ))}
+                  </div>
+                ) : (
+                  <Card.Img
+                    variant="top"
+                    src={project.imgPath}
+                    alt={`Screenshot of ${project.title}`}
+                    className="project-image-full"
+                  />
+                )}
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>{project.description}</Card.Text>
+                  <Button variant="primary" href={project.link} target="_blank">
+                    <BiLinkExternal /> &nbsp; View Project
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          ))}
+        </Row>
+      </Container>
+      <Footer className="mt-auto" />
     </div>
   );
 };
